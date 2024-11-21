9b0120a38b507240b6ec76597414b824a54d0361
cyq <caoyongqi@5i5j.com>
2024-11-21 18:04:12 +0800

    init

--- app/middware/__init__.py
+++ app/middware/__init__.py
@@ -1,1 +1,1 @@
-#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2024/6/7# @Author : cyq# @File : __init__.py# @Software: PyCharm# @Desc:CORS_ALLOW_ORIGINS = {    "allow_origins": ['*'],    "allow_credentials": True,    "allow_methods": ['*'],    "allow_headers": ['*'],}
+#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2024/6/7# @Author : cyq# @File : __init__.py# @Software: PyCharm# @Desc:CORS_ALLOW_ORIGINS = {    "allow_origins": ['*'],    "allow_credentials": True,    "allow_methods": ['*'],    "allow_headers": ['*'],}
