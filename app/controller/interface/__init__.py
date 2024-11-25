20151be7556bdf582474dcbd02a1499dbcb8f6c3
cyq <caoyongqi@5i5j.com>
2024-11-25 18:03:56 +0800

    init

--- app/controller/interface/__init__.py
+++ app/controller/interface/__init__.py
@@ -1,1 +1,1 @@
-#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2024/11/19# @Author : cyq# @File : __init__.py# @Software: PyCharm# @Desc:from . import interfaceCase,interfaceStep,interfaceTask,interfaceResult
+#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2024/11/19# @Author : cyq# @File : __init__.py# @Software: PyCharm# @Desc:from . import interfaceCase,interfaceApi,interfaceTask,interfaceResult
