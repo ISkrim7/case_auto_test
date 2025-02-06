31dfb3366ed53a889baebf07326312eb382ad587
cyq <caoyongqi@5i5j.com>
2025-02-06 18:02:31 +0800

    init

--- play/exception.py
+++ play/exception.py
@@ -1,1 +1,15 @@
-#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2024/8/12# @Author : cyq# @File : exception# @Software: PyCharm# @Desc:class APIAssertException(AssertionError):    ...class PlayAssertException(AssertionError):    ...class CBSLoginFail(Exception):    ...
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time : 2024/8/12
+# @Author : cyq
+# @File : exception
+# @Software: PyCharm
+# @Desc:
+
+
+class APIAssertException(AssertionError):
+    ...
+
+
+
+
--- play/methods/expect_method.py
+++ play/methods/expect_method.py
@@ -1,8 +1,11 @@
+import re
+
 from playwright.async_api import Page, expect, PageAssertions
 
 from app.model.ui import UICaseStepsModel, UIResultModel
 from play.extract import ExtractManager
 from play.methods.page_methods import PageMethods
+from play.writer import Writer
 from utils import GenerateTools, log
 from utils.io_sender import SocketSender
 
@@ -33,31 +36,52 @@ class ExpectMethod:
             "stepName": step.name,
             "assertOpt": "==",
             "extraValueType": "-",
+            "extraOpt": "-",
+            "extraValue": step.method.split(".")[-1],
+            "expect": step.value,
 
         }
-
 
         expect.set_options(timeout=1000)
         method = step.method.split(".")[-1]
         expect_value = await em.transform_target(step.value)
-        if expect_value:
-            await io.send(f">>expect_value: {expect_value}")
-        if isinstance(expect_value, (int, float)):
+        if expect_value and isinstance(expect_value, (int, float)):
             expect_value = str(expect_value)
+        assertInfo['expect'] = expect_value
+        await io.send(f"断言信息：{assertInfo}")
         try:
             # PageAssertions
-            if method in PageAssertions.__dict__:
-                await getattr(expect(page), method)(expect_value)
+            if method == "to_have_title":
+                await expect(page).to_have_title(expect_value)
+                assertInfo["actual"] = await page.title()
+            elif method == "to_have_url":
+                await expect(page).to_have_url(expect_value)
+                assertInfo["actual"] = page.url
             else:
                 # LocatorAssertions
                 locator = await PageMethods.get_locator(page, step)
                 expect_method = getattr(expect(locator), method)
                 await expect_method(expect_value) if step.value else await expect_method()
             assertInfo["result"] = True
-            case_result.asserts_info.append(assertInfo)
-            log.debug(case_result.asserts_info)
-        except Exception as e:
+        except AssertionError as e:
             assertInfo["result"] = False
-            assertInfo["actual"] = str(e)
+            assertInfo["actual"] = await ExpectMethod.get_error_value(e)
+            raise e
+        finally:
+            await Writer.write_assert_info(case_result, [assertInfo])
 
-        # [{"id": 1738547896726, "desc": "断言code", "type": "API", "actual": 0, "expect": "0", "result": true, "extraOpt": "jsonpath", "stepName": "test", "assertOpt": "==", "extraValue": "$.code", "extraValueType": "integer"}]
+    @staticmethod
+    async def get_error_value(e: AssertionError):
+        err = str(e)
+        if "Actual value:" in err:
+            # 正则表达式匹配 Actual value 和 Call log 之间的内容
+            pattern = r"Actual value:\s*(.*?)\s*Call log:"
+            match = re.search(pattern, err, re.DOTALL)
+            if match:
+                actual_value = match.group(1).strip()
+                return actual_value
+            else:
+                return ""
+        return ""
+
+
