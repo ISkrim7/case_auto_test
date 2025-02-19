9e06bfbcc932494fed67a6840d0172504469a37b
cyq <caoyongqi@5i5j.com>
2025-02-19 18:07:01 +0800

    init

--- app/model/interface/interfaceScriptDescModel.py
+++ app/model/interface/interfaceScriptDescModel.py
@@ -12,19 +12,3 @@ class InterfaceScriptDesc(BaseModel):
     desc = Column(String(100), nullable=True, comment="描述")
     example = Column(String(200), nullable=True, comment="例子")
     returnContent = Column(String(200), nullable=True, comment="返回")
-
-
-
-a = """
-  {
-    title: 'timestamp(day:str = None)-> str 获取时间戳',
-    subTitle: 'Func',
-    args: ['+1s', '-1s', '+1m', '-1m', '+1h', '-1h'],
-    description: '获取不同时间的时间戳',
-    example:
-      "例子: ts = timestamp() 返回当前时间戳 ts = timestamp('+1s') 返回1秒后的时间戳",
-    returnContent: ':return 1705114614000',
-  },
-  """
-
-
