76dcb9e725f3f25cd59924cab9ef676d1359bd87
cyq <caoyongqi345@gmail.com>
2025-03-29 12:40:58 +0800

    ui优化

--- play/methods/customize_method.py
+++ play/methods/customize_method.py
@@ -4,9 +4,8 @@ from playwright.async_api import Page
 from app.model.ui import UICaseStepsModel
 from play.extract import ExtractManager
 from play.methods.page_methods import PageMethods
-
+from play.starter import UIStarter
 from play.upload_files import png, xlsx
-from utils.io_sender import SocketSender
 from utils import MyLoguru
 
 log = MyLoguru().get_logger()
@@ -14,47 +13,46 @@ log = MyLoguru().get_logger()
 
 class CustomizeMethod:
     """
-    Page Method
+    Page Method 自定义封装
     """
 
     @staticmethod
-    async def invoke(page: Page, step: UICaseStepsModel, io: SocketSender, em: ExtractManager):
+    async def invoke(page: Page, step: UICaseStepsModel, starter: UIStarter, em: ExtractManager):
         """
         执行方法
         :param page:
         :param step:
-        :param io:SocketSender
+        :param starter:UIStarter
         :param em:ExtractManager
         :return:
         """
         try:
-            await getattr(CustomizeMethod, step.method)(page, step, io, em)
+            await getattr(CustomizeMethod, step.method)(page, step, starter, em)
         except Exception as e:
             raise e
 
     @staticmethod
-    async def wait(page: Page, step: UICaseStepsModel, io: SocketSender, em: ExtractManager):
+    async def wait(page: Page, step: UICaseStepsModel, starter: UIStarter):
         """
         执行方法
         :param page:
         :param step:
-        :param io:SocketSender
-        :param em:ExtractManager
+        :param starter:UIStarter
         :return:
         """
         try:
-            await io.send(f"wait {step.value} ...")
+            await starter.send(f"wait {step.value} ...")
             await page.wait_for_timeout(float(step.value))
         except Exception as e:
             raise e
 
     @staticmethod
-    async def get_attr(page: Page, step: UICaseStepsModel, io: SocketSender, em: ExtractManager):
+    async def get_attr(page: Page, step: UICaseStepsModel, starter: UIStarter, em: ExtractManager):
         """
         获取locator上的属性
         :param page:
         :param step:
-        :param io: SocketSender
+        :param starter: UIStarter
         :param em:ExtractManager
         :return:
         """
@@ -62,7 +60,7 @@ class CustomizeMethod:
             try:
                 name, value = step.value.split(";")
             except Exception:
-                await io.send(f"提取变量失败 ⚠️ :{step.value}")
+                await starter.send(f"提取变量失败 ⚠️ :{step.value}")
                 raise Exception(f"提取变量失败 ⚠️ :{step.value}")
             locator = await PageMethods.get_locator(page, step)
             attr = await locator.get_attribute(name)
@@ -70,19 +68,19 @@ class CustomizeMethod:
                 tempVariable = {
                     value.strip(): attr.strip()
                 }
-                await io.send(f"提取变量 ✅ {tempVariable}")
+                await starter.send(f"提取变量 ✅ {tempVariable}")
                 await em.add_var(key=step.value.strip(), value=attr.strip())
 
         except Exception as e:
             raise e
 
     @staticmethod
-    async def get_text(page: Page, step: UICaseStepsModel, io: SocketSender, em: ExtractManager):
+    async def get_text(page: Page, step: UICaseStepsModel, starter: UIStarter, em: ExtractManager):
         """
         获取locator上的文案
         :param page:
         :param step:
-        :param io:SocketSender
+        :param starter:UIStarter
         :param em:ExtractManager
         :return:
         """
@@ -93,7 +91,7 @@ class CustomizeMethod:
                 tempVariable = {
                     step.value.strip(): text.strip()
                 }
-                await io.send(f"提取变量 ✅ {tempVariable}")
+                await starter.send(f"提取变量 ✅ {tempVariable}")
                 await em.add_var(key=step.value.strip(), value=text.strip())
             else:
                 pass
@@ -101,12 +99,12 @@ class CustomizeMethod:
             raise e
 
     @staticmethod
-    async def upload_file(page: Page, step: UICaseStepsModel, io: SocketSender, **kwargs):
+    async def upload_file(page: Page, step: UICaseStepsModel, starter: UIStarter):
         """
         上传文件
         :param page:
         :param step:
-        :param io:SocketSender
+        :param starter:UIStarter
         :return:
         """
         try:
@@ -115,29 +113,20 @@ class CustomizeMethod:
                 await locator.set_input_files(xlsx)
             else:
                 await locator.set_input_files(png)
-            await io.send("上传附件 ✅")
+            await starter.send("上传附件 ✅")
         except Exception as e:
             raise e
 
     @staticmethod
-    async def to_reload(page: Page, **kwargs):
-        """
-        页面刷新
-        :param page:
-        :return:
-        """
-        await page.reload()
-
-    @staticmethod
     async def to_fill(page: Page, step: UICaseStepsModel,
-                      io: SocketSender, em: ExtractManager):
+                      starter: UIStarter, em: ExtractManager):
         """
         输入方法特殊处理
         fill 前 先清空
         查询变量提取。进行替换
         :param page:Page
         :param step:UICaseStepsModel
-        :param io:SocketSender
+        :param starter:UIStarter
         :param em:ExtractManager
         :return:
         """
@@ -145,54 +134,55 @@ class CustomizeMethod:
             locator = await PageMethods.get_locator(page, step)
             # 找到元素方法
             fill_value = await em.transform_target(step.value)
-            await io.send(f"输入 >> {fill_value}")
+            await starter.send(f"输入 >> {fill_value}")
             await locator.fill(str(fill_value))
         except Exception as e:
             raise e
 
     @staticmethod
     async def to_evaluate(page: Page, step: UICaseStepsModel,
-                          io: SocketSender, **kwargs):
+                          starter: UIStarter):
         """
         执行js
         :param page:
-        :param io:SocketSender
+        :param starter:UIStarter
         :param step: UICaseStepsModel
         :return:
         """
         try:
 
             result = await page.evaluate(step.value)
-            await io.send(f">> evaluate:  '{step.value}' result: {result}")
+            await starter.send(f">> evaluate:  '{step.value}' result: {result}")
         except Exception as e:
             raise e
 
     @staticmethod
-    async def to_wait(page: Page, step: UICaseStepsModel,
-                      io: SocketSender, **kwargs):
+    async def to_wait(page: Page,
+                      step: UICaseStepsModel,
+                      starter: UIStarter):
         """
         等一会
         :param page:
         :param step:
-        :param io
+        :param starter
         :return:
         """
         try:
             await page.wait_for_timeout(float(step.value))
-            await io.send(f"wait_for_timeout  {step.value} done")
+            await starter.send(f"wait_for_timeout  {step.value} done")
         except Exception as e:
             raise e
 
     @staticmethod
     async def to_goto(page: Page,
                       step: UICaseStepsModel | str,
-                      io: SocketSender,
+                      starter: UIStarter,
                       em: ExtractManager):
         """
 
         :param page:Page
         :param step: 跳转
-        :param io: SocketSender
+        :param starter: UIStarter
         :param em:ExtractManager
         :return:
 
@@ -201,7 +191,7 @@ class CustomizeMethod:
             url = await em.transform_target(step)
         else:
             url = await em.transform_target(step.value)
-        await io.send(f"执行 >> 打开地址:'{url}'")
+        await starter.send(f"执行 >> 打开地址:'{url}'")
         try:
             await page.goto(url, wait_until="load")
             await page.wait_for_load_state()
@@ -211,4 +201,3 @@ class CustomizeMethod:
 
 
 CustomizeMethods = [name for name, member in inspect.getmembers(CustomizeMethod, predicate=inspect.isfunction)]
-print(CustomizeMethods)
