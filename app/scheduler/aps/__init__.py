f932fecc93982885b394ee4cfac7ea0c7d02478d
hinn <570096125@qq.com>
2025-06-19 09:27:12 +0800

    调度任务日志优化

--- app/scheduler/aps/__init__.py
+++ app/scheduler/aps/__init__.py
@@ -1,1 +1,1 @@
-#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2025/4/7# @Author : cyq# @File : __init__.py# @Software: PyCharm# @Desc:
+#!/usr/bin/env python# -*- coding:utf-8 -*-# @Time : 2025/4/7# @Author : cyq# @File : __init__.py# @Software: PyCharm# @Desc:from .base import BaseSchedulerfrom .tasks import run_heartbeat__all__ = ["BaseScheduler", "run_heartbeat"]
