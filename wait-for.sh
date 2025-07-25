e4001f747845b5f803f61ce8f879b9bd61909a7a
hinn <570096125@qq.com>
2025-07-25 15:57:35 +0800

    配置

--- wait-for.sh
+++ wait-for.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-until mysqladmin ping -h "127.0.0.1" -P "3306" -u "root" -p "sdkjfhsdkjhfsdkhfksd" --silent; do
+until mysqladmin ping -h "127.0.0.1" -P "127.0.0.1" -u "root" -p "127.0.0.1" --silent; do
     echo "Waiting for MySQL to be available..."
     sleep 1
 done
 
-until redis-cli -h "127.0.0.1" ping; do
+#until redis-cli -h "127.0.0.1" ping; do
+until redis-cli -h "127.0.0.1" -p "127.0.0.1" -a "127.0.0.1" ping; do
     echo "Waiting for Redis to be available..."
     sleep 1
 done
